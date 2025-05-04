0ecaa2147c022a92f65d1a66f6707f2f2a57a869
soso akemi <sophiaakemi1@gmail.com>
2025-05-04 23:06:43 +0000

    Cálculo do Volume da Esfera

---
+++ Esfera/esfera.js
@@ -0,0 +1,15 @@
+// Função para calcular o volume da esfera
+function calcularVolumeEsfera(raio) {
+    const pi = 3.14159;
+    const volume = (4.0 / 3) * pi * Math.pow(raio, 3); // Cálculo do volume
+    return volume;
+}
+
+// Leitura do valor do raio
+const raio = parseFloat(prompt("Digite o valor do raio da esfera:"));
+
+// Cálculo do volume
+const volume = calcularVolumeEsfera(raio);
+
+// Saída formatada do resultado
+console.log(`VOLUME = ${volume.toFixed(3)}`);
